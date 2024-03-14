9d288afd0b4d739838e3364f440df134eef327b4
jaroslav <jotradovec@e1133.co>
2024-03-14 10:07:50 +0100

    changing Car model

--- cars/models.py
+++ cars/models.py
@@ -3,3 +3,5 @@ from django.db import models
 
 class Car(models.Model):
     name = models.CharField(max_length=255)
+    maximum_speed = models.IntegerField(null=True)
+    acquired_date = models.DateField(null=True)
