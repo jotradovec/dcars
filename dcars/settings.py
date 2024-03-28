d9d13ee6a182523ffaa58a91f262f237b6319674
jaroslav <jotradovec@e1133.co>
2024-03-28 16:05:10 +0100

    Adding 404 page

--- dcars/settings.py
+++ dcars/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--@(!mnyl@q5gh&@zfh1@p0+gnm)59rz)z_c9x8omcnl3*%m&%*"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
