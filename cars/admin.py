93fcf95b7ebb689e4af195b61fe6c44f78e5f747
jaroslav <jotradovec@e1133.co>
2024-03-29 19:55:36 +0100

    Adding Car Admin display

--- cars/admin.py
+++ cars/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import Car
 
-admin.site.register(Car)
+
+class CarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'maximum_speed', 'acquired_date')
+
+
+admin.site.register(Car, CarAdmin)
