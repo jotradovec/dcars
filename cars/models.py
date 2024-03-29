3e22ccf8ad4fed99b5937feff173754160ad5b6a
jaroslav <jotradovec@e1133.co>
2024-03-29 20:49:27 +0100

    Adding blank od maximum_speed field

--- cars/models.py
+++ cars/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Car(models.Model):
     name = models.CharField(max_length=255)
-    maximum_speed = models.IntegerField(null=True)
+    maximum_speed = models.IntegerField(null=True, blank=True)
     acquired_date = models.DateField(null=True)
 
     def __str__(self):
