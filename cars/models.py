cf0a5ded8a09fff4e6764d7f8e7163d98684aab6
jaroslav <jotradovec@e1133.co>
2024-04-03 13:31:42 +0200

    Adding operational field in the Car model

--- cars/models.py
+++ cars/models.py
@@ -5,6 +5,7 @@ class Car(models.Model):
     name = models.CharField(max_length=255)
     maximum_speed = models.IntegerField(null=True, blank=True)
     acquired_date = models.DateField(null=True)
+    operational = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
